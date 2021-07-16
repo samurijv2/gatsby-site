57d3c9647ffbf546737118c3509fe88566cd4237
Sam Waymire <sam@samwaymire.com>
2021-07-16 10:46:33 -0400

    fixed blog page responsiveness

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,8 @@
 import * as React from 'react'
-import { graphql, Link } from 'gatsby'
 import Layout from '../components/layout'
-import { blogDate, blogLink, blogPostList } from '../components/layout.module.css'
 import { SEO } from '../components/SEO'
+import { graphql, Link } from 'gatsby'
+import { blogDate, blogLink, blogPostList } from '../components/layout.module.css'
 
 export const query = graphql`
 query BlogPageQuery {
@@ -20,21 +20,22 @@ query BlogPageQuery {
   }
 `
 
-const BlogPage = ({ data }) => {    
+const BlogPage = ({data}) => {    
   return (  
     <Layout pageTitle="✍️ Blog">
        <SEO
        title="Blog - Sam Waymire" 
        description="Welcome to my blog"
       />
-      <ul className={blogPostList}>
+            <ul className={blogPostList}>
           {data.allSanityPost.edges.map(posts => (
             <li key={posts.node.title} style={{marginBottom:"0.3rem"}}>
             <Link className={blogLink} to={posts.node.slug.current}>{posts.node.title}</Link><span className={blogDate}>{posts.node.publishedAt}</span>    
-          </li>
+            </li>
           ))
           }    
       </ul>
+
     </Layout>
   )
 } 
