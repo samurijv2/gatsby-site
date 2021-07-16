0ebad4750fa5fc582cb8a8f3cd630e5c8ca03ccb
samurijv2 <sam@samwaymire.com>
2021-07-16 14:57:58 -0400

    enabled caching & incremental builds for netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
+    `gatsby-plugin-netlify-cache`,
     "gatsby-plugin-catch-links",
     "gatsby-plugin-react-helmet",
     'gatsby-plugin-robots-txt',
